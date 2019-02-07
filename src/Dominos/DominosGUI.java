af646af5b172d2d9d38d3574b9cc60b9f4b11cc1
craigparry <parryc@unm.edu>
2019-02-06 17:50:41 -0700

    drawing dominos in two rows now, need to create all dominos dynamically

--- src/Dominos/DominosGUI.java
+++ src/Dominos/DominosGUI.java
@@ -1,97 +1,110 @@
 package Dominos;
 
-import javafx.application.Application;
-import java.time.Duration;
-
-import javafx.stage.Stage;
-import javafx.scene.control.*;
-import javafx.scene.*;
-import javafx.animation.AnimationTimer;
-import javafx.scene.Scene;
-import javafx.scene.canvas.*;
-import javafx.scene.layout.*;
-import javafx.scene.paint.*;
-//import javafx.scene.paint.Color;
-import javafx.animation.AnimationTimer;
-import javafx.application.Application;
 import javafx.geometry.Pos;
-import javafx.stage.*;
-import javafx.scene.control.*;
-import javafx.scene.*;
-import javafx.scene.layout.*;
 import javafx.scene.canvas.*;
 import javafx.scene.paint.*;
-import javafx.event.*;
-import java.util.Map;
-import java.util.HashMap;
-import java.util.Set;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.*;
+//import javafx.scene.layout.VBox;
+import javafx.scene.control.*;
 import java.util.*;
 
 public class DominosGUI extends Application{
+    Canvas canvas;
+    GraphicsContext gc;
 
-public static void main(String[] args) { launch(args);}
+    public Canvas drawDomino(int num){
+        Canvas canvas;
+        GraphicsContext gc;
+        canvas = new Canvas(40,40);
+        gc = canvas.getGraphicsContext2D();
+        gc.setFill ( Color . WHITE);
+        gc.fillRect(0,0,canvas.getWidth(),canvas.getHeight());
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(4);
+        gc.strokeRect(0,0,canvas.getWidth(),canvas.getHeight());
 
-@Override
-    public void start(Stage primaryStage){
-        MainGameLoop game = new MainGameLoop();
-        game.newGame();
-        Map<String,Canvas> dominoMap = new HashMap<>();
-        Set<Canvas> dominoSet = new HashSet<>();
+        switch(num){
 
-
-
-
-        for(int i = 0; i<=6; i++){
-            Canvas temp = new Canvas(50, 25);
-            GraphicsContext gc = temp.getGraphicsContext2D();
-            gc.setFill(Color.WHITE);
-            gc.fillRect(0,0,temp.getWidth(),temp.getHeight());
-            gc.setStroke(Color.BLACK);
-
-
-
+            case 1: gc.setStroke(Color.BLACK);
+                gc.strokeOval(19,19,2,2);
+                break;
+            case 2: gc.setStroke(Color.BLACK);
+                gc.strokeOval(10,10,2,2);
+                gc.strokeOval(28,28,2,2);
+                break;
+            case 3: gc.setStroke(Color.BLACK);
+                gc.strokeOval(19,19,2,2);
+                gc.strokeOval(10,10,2,2);
+                gc.strokeOval(28,28,2,2);
+                break;
+            case 4: gc.setStroke(Color.BLACK);
+                gc.strokeOval(10,10,2,2);
+                gc.strokeOval(28,28,2,2);
+                gc.strokeOval(28,10,2,2);
+                gc.strokeOval(10,28,2,2);
+                break;
+            case 5: gc.setStroke(Color.BLACK);
+                gc.strokeOval(19,19,2,2);
+                gc.strokeOval(10,10,2,2);
+                gc.strokeOval(28,28,2,2);
+                gc.strokeOval(28,10,2,2);
+                gc.strokeOval(10,28,2,2);
+                break;
+            case 6: gc.setStroke(Color.BLACK);
+                gc.strokeOval(10,19,2,2);
+                gc.strokeOval(28,19,2,2);
+                gc.strokeOval(10,10,2,2);
+                gc.strokeOval(28,28,2,2);
+                gc.strokeOval(28,10,2,2);
+                gc.strokeOval(10,28,2,2);
+                break;
+            default: ;
+                break;
         }
 
-
-
-        for(Dominos d: game.getGrave()){
-
-        }
-
-        primaryStage.setTitle("DominosGame");
-
-//        HBox topRow = new HBox();
-//        HBox botRow = new HBox();
-        // probably wnat gridpane instead
-        GridPane topRow = new GridPane();
-        GridPane botRow = new GridPane();
-
-
-        VBox screen = new VBox();
-
-        Label text = new Label();
-        Label text2 = new Label("Hello");
-        topRow.getChildren().add(text);
-        botRow.getChildren().add(text2);
-        screen.getChildren().addAll(topRow,botRow);
-        primaryStage.setScene(new Scene(screen, 200,100));
-        primaryStage.show();
-
-        AnimationTimer a = new AnimationTimer(){
-            private long startTime = -1;
-
-            @Override
-            public void handle(long now){
-                if(startTime <0){
-                    startTime = now;
-                }
-                Duration elapsed = Duration.ofNanos(now - startTime);
-                text.setText(String.format("elapsed time %d:%02d",
-                        elapsed.toMinutesPart(),
-                        elapsed.toSecondsPart()));
-
-            }
-        };
-        a.start();
+        return canvas;
     }
+
+
+    @Override
+    public void start ( Stage stage ) {
+        stage.setTitle("Canvas Drawing Demo");
+        BorderPane screen = new BorderPane();
+        VBox vbox = new VBox(1);
+        HBox topRow = new HBox(1);
+        HBox botRow = new HBox(1);
+        HBox bigDomino = new HBox();
+
+        Map<String,HBox> DominoMap = new HashMap<>();
+
+
+        Canvas domino = drawDomino(6);
+        Canvas domino2 = drawDomino( 7);
+        bigDomino.getChildren().addAll(drawDomino(1),drawDomino(1));
+
+        DominoMap.put("1:2",bigDomino);
+        HBox temp = new HBox();
+        temp.getChildren().addAll(drawDomino(0),drawDomino(0));
+        DominoMap.put("0:0",temp);
+
+
+        topRow.getChildren().addAll(new Label("This is top"),domino);
+        botRow.getChildren().addAll(drawDomino(6), drawDomino(2),domino2,DominoMap.get("1:2"),DominoMap.get("0:0"));
+        vbox.getChildren().addAll(topRow,botRow);
+      //  vbox.getChildren().addAll(new Label("This is a "), new Button("is"), new Button("going"),drawDomino(1));
+        BorderPane.setAlignment(vbox, Pos.CENTER);
+        screen.setCenter(vbox);
+
+
+        stage.setScene(new Scene(new StackPane(screen)));
+        stage.show();
+    }
+
+    public static void main (String[] args){
+        launch(args);
+    }
+
 }
--- src/Dominos/DrawDomino.java
+++ src/Dominos/DrawDomino.java
@@ -1,66 +1,69 @@
-package Dominos;
-
-import javafx.scene.canvas.*;
-import javafx.scene.paint.*;
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.layout.*;
-public class DrawDomino extends Canvas{
-    private Canvas canvas;
-    private GraphicsContext gc;
-
-    public DrawDomino(int num){
-        canvas = new Canvas(40,40);
-        gc = canvas.getGraphicsContext2D();
-        gc.setFill ( Color . WHITE);
-        gc.fillRect(0,0,canvas.getWidth(),canvas.getHeight());
-        gc.setStroke(Color.BLACK);
-        gc.setLineWidth(4);
-        gc.strokeRect(0,0,canvas.getWidth(),canvas.getHeight());
-
-        switch(num){
-
-            case 1: gc.setStroke(Color.BLACK);
-                    gc.strokeOval(19,19,2,2);
-            break;
-            case 2: gc.setStroke(Color.BLACK);
-                    gc.strokeOval(10,10,2,2);
-                    gc.strokeOval(28,28,2,2);
-            break;
-            case 3: gc.setStroke(Color.BLACK);
-                    gc.strokeOval(19,19,2,2);
-                    gc.strokeOval(10,10,2,2);
-                    gc.strokeOval(28,28,2,2);
-            break;
-            case 4: gc.setStroke(Color.BLACK);
-                    gc.strokeOval(10,10,2,2);
-                    gc.strokeOval(28,28,2,2);
-                    gc.strokeOval(28,10,2,2);
-                    gc.strokeOval(10,28,2,2);
-            break;
-            case 5: gc.setStroke(Color.BLACK);
-                    gc.strokeOval(19,19,2,2);
-                    gc.strokeOval(10,10,2,2);
-                    gc.strokeOval(28,28,2,2);
-                    gc.strokeOval(28,10,2,2);
-                    gc.strokeOval(10,28,2,2);
-            break;
-            case 6: gc.setStroke(Color.BLACK);
-                    gc.strokeOval(10,19,2,2);
-                    gc.strokeOval(28,19,2,2);
-                    gc.strokeOval(10,10,2,2);
-                    gc.strokeOval(28,28,2,2);
-                    gc.strokeOval(28,10,2,2);
-                    gc.strokeOval(10,28,2,2);
-            break;
-            default:
-            break;
-        }
-
-    }
-
-
+//package Dominos;
+//
+//import javafx.scene.canvas.*;
+//import javafx.scene.paint.*;
+//import javafx.application.Application;
+//import javafx.scene.Scene;
+//import javafx.stage.Stage;
+//import javafx.scene.layout.*;
+//public class DrawDomino extends Application{
+//    Canvas canvas;
+//    GraphicsContext gc;
+//
+//    public Canvas drawDomino(int num){
+//        Canvas canvas;
+//        GraphicsContext gc;
+//        canvas = new Canvas(40,40);
+//        gc = canvas.getGraphicsContext2D();
+//        gc.setFill ( Color . WHITE);
+//        gc.fillRect(0,0,canvas.getWidth(),canvas.getHeight());
+//        gc.setStroke(Color.BLACK);
+//        gc.setLineWidth(4);
+//        gc.strokeRect(0,0,canvas.getWidth(),canvas.getHeight());
+//
+//        switch(num){
+//
+//            case 1: gc.setStroke(Color.BLACK);
+//                    gc.strokeOval(19,19,2,2);
+//            break;
+//            case 2: gc.setStroke(Color.BLACK);
+//                    gc.strokeOval(10,10,2,2);
+//                    gc.strokeOval(28,28,2,2);
+//            break;
+//            case 3: gc.setStroke(Color.BLACK);
+//                    gc.strokeOval(19,19,2,2);
+//                    gc.strokeOval(10,10,2,2);
+//                    gc.strokeOval(28,28,2,2);
+//            break;
+//            case 4: gc.setStroke(Color.BLACK);
+//                    gc.strokeOval(10,10,2,2);
+//                    gc.strokeOval(28,28,2,2);
+//                    gc.strokeOval(28,10,2,2);
+//                    gc.strokeOval(10,28,2,2);
+//            break;
+//            case 5: gc.setStroke(Color.BLACK);
+//                    gc.strokeOval(19,19,2,2);
+//                    gc.strokeOval(10,10,2,2);
+//                    gc.strokeOval(28,28,2,2);
+//                    gc.strokeOval(28,10,2,2);
+//                    gc.strokeOval(10,28,2,2);
+//            break;
+//            case 6: gc.setStroke(Color.BLACK);
+//                    gc.strokeOval(10,19,2,2);
+//                    gc.strokeOval(28,19,2,2);
+//                    gc.strokeOval(10,10,2,2);
+//                    gc.strokeOval(28,28,2,2);
+//                    gc.strokeOval(28,10,2,2);
+//                    gc.strokeOval(10,28,2,2);
+//            break;
+//            default: ;
+//            break;
+//        }
+//
+//        return canvas;
+//    }
+//
+//
 //    @Override
 //    public void start ( Stage stage ) {
 //        stage . setTitle (" Canvas Drawing Demo ");
@@ -71,5 +74,5 @@ public class DrawDomino extends Canvas{
 //    }
 //
 //    public static void main ( String [] args ) { launch ( args ); }
-
-}
+//
+//}
