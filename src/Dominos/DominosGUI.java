819ec9f9ef47ec0130fa04458809d5330d3566c0
craigparry <parryc@unm.edu>
2019-02-06 20:22:30 -0700

    yay got pieces to show up on the board that have listeners attached and all my pieces are stored in a map now

--- src/Dominos/DominosGUI.java
+++ src/Dominos/DominosGUI.java
@@ -10,10 +10,13 @@ import javafx.scene.layout.*;
 //import javafx.scene.layout.VBox;
 import javafx.scene.control.*;
 import java.util.*;
+import javafx.scene.input.MouseEvent;
 
 public class DominosGUI extends Application{
-    Canvas canvas;
-    GraphicsContext gc;
+    private Canvas canvas;
+    private GraphicsContext gc;
+    private MainGameLoop game = new MainGameLoop();
+
 
     public Canvas drawDomino(int num){
         Canvas canvas;
@@ -71,28 +74,53 @@ public class DominosGUI extends Application{
 
     @Override
     public void start ( Stage stage ) {
-        stage.setTitle("Canvas Drawing Demo");
+        stage.setTitle("Domino Game");
         BorderPane screen = new BorderPane();
         VBox vbox = new VBox(1);
         HBox topRow = new HBox(1);
         HBox botRow = new HBox(1);
         HBox bigDomino = new HBox();
 
-        Map<String,HBox> DominoMap = new HashMap<>();
+
+        Map<String,HBox> dominoMap = new HashMap<>();
 
 
-        Canvas domino = drawDomino(6);
-        Canvas domino2 = drawDomino( 7);
-        bigDomino.getChildren().addAll(drawDomino(1),drawDomino(1));
+        for(Dominos d: game.getGrave()){
+            HBox temp = new HBox();
+            temp.getChildren().addAll(drawDomino(d.getLeftSide()),drawDomino(d.getRightSide()));
 
-        DominoMap.put("1:2",bigDomino);
-        HBox temp = new HBox();
-        temp.getChildren().addAll(drawDomino(0),drawDomino(0));
-        DominoMap.put("0:0",temp);
+            temp.addEventHandler( MouseEvent . MOUSE_PRESSED , event -> {
+                System . out . println (" pressed "
+                        + event . getX () + " " + event . getY ());
+                System.out.println(d.toString());
+            });
+            dominoMap.put(d.toString(),temp);
+        }
+//        Map<String,Button> DominoMap2 = new HashMap<>();
+//        Button dominoButton = new Button();
+//        dominoButton.setGraphic(drawDomino(1));
 
 
-        topRow.getChildren().addAll(new Label("This is top"),domino);
-        botRow.getChildren().addAll(drawDomino(6), drawDomino(2),domino2,DominoMap.get("1:2"),DominoMap.get("0:0"));
+
+//        Canvas domino = drawDomino(6);
+//        Canvas domino2 = drawDomino( 7);
+//        bigDomino.getChildren().addAll(drawDomino(1),drawDomino(1));
+
+//        DominoMap.put("1:2",bigDomino);
+//        HBox temp = new HBox();
+//        temp.addEventHandler( MouseEvent . MOUSE_PRESSED , event -> {
+//            System . out . println (" pressed "
+//                    + event . getX () + " " + event . getY ());
+//        });
+
+//        temp.getChildren().addAll(drawDomino(0),drawDomino(0));
+//        DominoMap.put("0:0",temp);
+
+        HBox tray = new HBox();
+
+        topRow.getChildren().addAll(new Label("This is top"));
+//        botRow.getChildren().addAll(drawDomino(6), drawDomino(2),domino2,DominoMap.get("1:2"),DominoMap.get("0:0"));
+        botRow.getChildren().addAll(dominoMap.get("1:1"),dominoMap.get("1:2"));
         vbox.getChildren().addAll(topRow,botRow);
       //  vbox.getChildren().addAll(new Label("This is a "), new Button("is"), new Button("going"),drawDomino(1));
         BorderPane.setAlignment(vbox, Pos.CENTER);
